94ae3924288b66ffb8abfd0f4432ea0aaa759d5f
jaindriss <jaindrissosajsd@gmail.com>
2023-08-02 17:36:40 -0500

    feat: add save method

--- src/user/domain/user.repository.ts
+++ src/user/domain/user.repository.ts
@@ -1,5 +1,8 @@
+import { UserDTO } from "../application/user.dto";
 import { User } from "./user.interfaces";
 
 export interface userRepository {
   getAllUser(): Promise<User[] | null>;
+
+  save(data: UserDTO): Promise<unknown>;
 }
