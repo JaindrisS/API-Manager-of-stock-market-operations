2e653103ef1f396e5a705505cd2897574fe5663f
jaindriss <jaindrissosajsd@gmail.com>
2023-08-02 17:42:38 -0500

    feat: add method create user
    add create user method to UserService class

--- src/user/application/user.service.ts
+++ src/user/application/user.service.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { HttpResponse } from "../../shared/response/httpResponse";
 import { userRepository } from "../domain/user.repository";
+import { UserDTO } from "./user.dto";
+import * as bcrypt from "bcrypt";
 
 export class UserService {
   private readonly userRepository: userRepository;
@@ -11,13 +13,31 @@ export class UserService {
     this.httpResponse = httpResponse;
   }
 
-  async getAllUser(_req: Request, res: Response) {
+  async getAllUser(res: Response) {
     try {
       const response = await this.userRepository.getAllUser();
       if (response?.length === 0) {
         return this.httpResponse.NotFound(res, "No data");
       }
       return this.httpResponse.OK(res, response);
+    } catch (error) {
+      return this.httpResponse.Error(res, error);
+    }
+  }
+
+  async createUser(res: Response, data: UserDTO) {
+    try {
+      const { name, password, email } = data;
+      const hastPass = bcrypt.hashSync(password, 10);
+      const info = {
+        password: hastPass.toString(),
+        name: name.toUpperCase(),
+        email: email,
+      };
+
+      const user = await this.userRepository.save(info);
+
+      return this.httpResponse.Created(res, user);
     } catch (error) {
       return this.httpResponse.Error(res, error);
     }
