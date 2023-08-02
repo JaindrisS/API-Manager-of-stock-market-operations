35b026f3bb9df55d09b23ec1e848dd99f721a90d
jaindriss <jaindrissosajsd@gmail.com>
2023-08-02 17:44:47 -0500

    feat: add save method
    this method is used to save user in the database

--- src/user/infrastructure/user-repository/mongo/DbUser.Repository.ts
+++ src/user/infrastructure/user-repository/mongo/DbUser.Repository.ts
@@ -1,10 +1,21 @@
-import { userModel } from "../../../domain/user.model";
+import { userModel } from "./user.model";
 import { User } from "../../../domain/user.interfaces";
 import { userRepository } from "../../../domain/user.repository";
+import { UserDTO } from "../../../application/user.dto";
 
 export class UserRepository implements userRepository {
   async getAllUser(): Promise<User[] | null> {
     const data = await userModel.find({ status: true });
     return data;
+  }
+
+  async save(data: UserDTO): Promise<unknown> {
+    try {
+      const response = new userModel(data);
+      await response.save();
+      return response;
+    } catch (error) {
+      return console.log(error);
+    }
   }
 }
