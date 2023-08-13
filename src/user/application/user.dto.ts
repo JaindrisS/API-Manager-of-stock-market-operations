4210632549e3420555e4d03d65e31586f9b603af
jaindriss <jaindrissosajsd@gmail.com>
2023-08-13 18:34:59 -0500

    feat: add validation to properties

---
+++ src/user/application/user.dto.ts
@@ -0,0 +1,32 @@
+import {
+  IsNotEmpty,
+  IsString,
+  IsEmail,
+  IsOptional,
+  Length,
+} from "class-validator";
+import { IsEmailAlreadyExist } from "./user.validations";
+export enum RoleType {
+  USER = "USER",
+  ADMIN = "ADMIN",
+}
+
+export class UserDTO {
+  @IsNotEmpty()
+  name!: string;
+
+  @IsEmailAlreadyExist({ groups: ["IsEmailAlreadyExist"] })
+  @IsNotEmpty()
+  @IsEmail()
+  email!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(6, 12, {
+    message: "The password must be between 6 and 12 characters long",
+  })
+  password!: string;
+
+  @IsOptional()
+  img?: string;
+}
