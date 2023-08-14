eee42429fda5d3216df7ec7dbca938fff735acfd
jaindriss <jaindrissosajsd@gmail.com>
2023-08-13 20:09:01 -0500

    feat: add dependency for usermiddleware

--- src/user/infrastructure/dependecies.ts
+++ src/user/infrastructure/dependecies.ts
@@ -2,11 +2,15 @@ import { HttpResponse } from "../../shared/response/httpResponse";
 import { UserService } from "../application/user.service";
 import { UserController } from "./rest-api/user.controller";
 import { UserRepository } from "../infrastructure/user-repository/mongo/DbUser.Repository";
+import { UserMiddleware } from "./user.middleware";
 
-const hhtpResponse = new HttpResponse();
+export const httpResponse = new HttpResponse();
 
 const userRepository = new UserRepository();
 
-const userService = new UserService(userRepository, hhtpResponse);
+const userService = new UserService(userRepository, httpResponse);
 
-export const userController = new UserController(userService, hhtpResponse);
+export const userController = new UserController(userService, httpResponse);
+
+
+export const userMiddleware = new UserMiddleware()
