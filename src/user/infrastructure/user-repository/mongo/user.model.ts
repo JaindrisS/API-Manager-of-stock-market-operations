242073fa7b5eb99eff8827d702c2e3dd2f1da5e8
jaindriss <jaindrissosajsd@gmail.com>
2023-08-13 20:13:37 -0500

    usermodel

---
+++ src/user/infrastructure/user-repository/mongo/user.model.ts
@@ -0,0 +1,45 @@
+import { model, Schema } from "mongoose";
+import { User } from "../../../domain/user.interfaces";
+
+const userSchema = new Schema<User>(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+    },
+
+    resetPassword: {
+      type: String,
+    },
+
+    status: {
+      type: Boolean,
+      default: true,
+    },
+
+    rol: {
+      type: String,
+      default: "USER",
+    },
+
+    img: {
+      type: String,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+export const userModel = model("User", userSchema);
