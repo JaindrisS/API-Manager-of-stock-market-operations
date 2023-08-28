21920cef1777a297094f1eaa7a09ac52011d3756
jaindriss <jaindrissosajsd@gmail.com>
2023-08-27 22:02:31 -0500

    feat: add and export instances of userDto and updateUserDto

--- src/user/infrastructure/dependecies.ts
+++ src/user/infrastructure/dependecies.ts
@@ -3,6 +3,7 @@ import { UserService } from "../application/user.service";
 import { UserController } from "./rest-api/user.controller";
 import { UserRepository } from "../infrastructure/user-repository/mongo/DbUser.Repository";
 import { UserMiddleware } from "./user.middleware";
+import { UpdateUserDto, UserDTO } from "../application/user.dto";
 
 export const httpResponse = new HttpResponse();
 
@@ -12,5 +13,8 @@ const userService = new UserService(userRepository, httpResponse);
 
 export const userController = new UserController(userService, httpResponse);
 
+export const updateUserDto = new UpdateUserDto();
 
-export const userMiddleware = new UserMiddleware()
+export const userDTO = new UserDTO();
+
+export const userMiddleware = new UserMiddleware();
