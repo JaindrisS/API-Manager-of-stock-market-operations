64bc282fe4e9a4c04a62d420aba7812263620dd2
jaindriss <jaindrissosajsd@gmail.com>
2023-08-27 22:05:55 -0500

    feat: add route to update user
    feat: add middleware

--- src/user/infrastructure/rest-api/user.router.ts
+++ src/user/infrastructure/rest-api/user.router.ts
@@ -1,7 +1,7 @@
 import { BaseRouter } from "../../../shared/router/router";
 import { userController } from "../dependecies";
 import { UserController } from "../rest-api/user.controller";
-import { userMiddleware } from "../dependecies";
+import { userMiddleware, userDTO, updateUserDto } from "../dependecies";
 import { UserMiddleware } from "../user.middleware";
 
 export class UserRouters extends BaseRouter<UserController, UserMiddleware> {
@@ -13,8 +13,15 @@ export class UserRouters extends BaseRouter<UserController, UserMiddleware> {
     this.router.get("/users", (req, res) => this.controller.getUsers(req, res));
     this.router.post(
       "/users",
-      (req, res, next) => this.middleware.userValidator(req, res, next),
+      (req, res, next) =>
+        this.middleware.createValidator(req, res, next, userDTO),
       (req, res) => this.controller.createUser(req, res)
+    );
+    this.router.put(
+      "/users/:id",
+      (req, res, next) =>
+        this.middleware.createValidator(req, res, next, updateUserDto),
+      (req, res) => this.controller.updateUser(req, res)
     );
   }
 }
