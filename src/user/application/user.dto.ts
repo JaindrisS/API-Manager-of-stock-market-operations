273057709c68644067fe59ab1e675193d708d79d
jaindriss <jaindrissosajsd@gmail.com>
2023-08-27 21:58:08 -0500

    feat: add user dto to update

--- src/user/application/user.dto.ts
+++ src/user/application/user.dto.ts
@@ -30,3 +30,26 @@ export class UserDTO {
   @IsOptional()
   img?: string;
 }
+
+export class UpdateUserDto {
+  @IsNotEmpty()
+  @IsOptional()
+  name!: string | undefined;
+
+  @IsOptional()
+  @IsEmailAlreadyExist({ groups: ["IsEmailAlreadyExist"] })
+  @IsNotEmpty()
+  @IsEmail()
+  email!: string | undefined;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
+  @Length(6, 12, {
+    message: "The password must be between 6 and 12 characters long",
+  })
+  password!: string | undefined;
+
+  @IsOptional()
+  img?: string;
+}
