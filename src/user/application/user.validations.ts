5ccd9012f8abb3aadce0e44e4c00ac49e512c8c8
jaindriss <jaindrissosajsd@gmail.com>
2023-08-28 22:10:03 -0500

    feat: add function MongoIdDoesNotExist
    add function MongoIdDoesNotExist to register decorator mongoIdDoesNotExistConstrain

--- src/user/application/user.validations.ts
+++ src/user/application/user.validations.ts
@@ -3,15 +3,20 @@ import { registerDecorator, ValidationOptions } from "class-validator";
 import { HttpResponse } from "../../shared/response/httpResponse";
 import { UserService } from "../application/user.service";
 import { UserRepository } from "../infrastructure/user-repository/mongo/DbUser.Repository";
-import { IsEmailAlreadyExistConstrain } from "../infrastructure/validations/user.validations.constraint";
-
-
+import {
+  IsEmailAlreadyExistConstrain,
+  MongoIdDoesNotExistConstrain,
+} from "../infrastructure/validations/user.validations.constraint";
 
 const httpResponse = new HttpResponse();
 const userRepository = new UserRepository();
 const userService = new UserService(userRepository, httpResponse);
 
 const isEmailAlreadyExistConstrain = new IsEmailAlreadyExistConstrain(
+  userService
+);
+
+const mongoIdDoesNotExistConstrain = new MongoIdDoesNotExistConstrain(
   userService
 );
 
@@ -22,6 +27,17 @@ export const IsEmailAlreadyExist = (validationOptions?: ValidationOptions) => {
       propertyName: propertyName,
       options: validationOptions,
       validator: isEmailAlreadyExistConstrain,
+    });
+  };
+};
+
+export const MongoIdDoesNotExist = (validationOptions?: ValidationOptions) => {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: mongoIdDoesNotExistConstrain,
     });
   };
 };
