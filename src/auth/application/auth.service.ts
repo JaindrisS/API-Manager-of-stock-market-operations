0077cbfa24e4c484ab2560cb3e69804014a85863
jaindriss <jaindrissosajsd@gmail.com>
2023-09-02 20:52:42 -0500

    feat: add authservice class
    add user login method

---
+++ src/auth/application/auth.service.ts
@@ -0,0 +1,38 @@
+import { Response } from "express";
+import * as bcrypt from "bcrypt";
+import { HttpResponse } from "../../shared/response/httpResponse";
+import { authRepository } from "../domain/auth.repository";
+
+export class AuthService {
+  private readonly authRepository: authRepository;
+  private readonly httpResponse: HttpResponse;
+
+  constructor(authRepository: authRepository, httpResponse: HttpResponse) {
+    this.authRepository = authRepository;
+    this.httpResponse = httpResponse;
+  }
+
+  async login(email: string, password: string, res: Response) {
+    try {
+      const user = await this.authRepository.getUser(email);
+
+      if (user?.password === undefined || user?.email === undefined) {
+        return this.httpResponse.BadRequest(res, "Invalid email or password");
+      }
+
+      const comparePassword = bcrypt.compareSync(password, user?.password);
+
+      if (!user?.status || !user || !comparePassword) {
+        return this.httpResponse.BadRequest(res, "Invalid email or password");
+      }
+
+      return this.httpResponse.OK(res, {
+        msg: "Login ok",
+        user,
+        token: "token",
+      });
+    } catch (error) {
+      return this.httpResponse.Error(res, error);
+    }
+  }
+}
