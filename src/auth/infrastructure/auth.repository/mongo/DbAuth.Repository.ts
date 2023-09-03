a93eca130fb6e14fc1b96a90f8f68a6341d660ca
jaindriss <jaindrissosajsd@gmail.com>
2023-09-02 21:07:30 -0500

    feat: add authRepository class
    add method getUser

---
+++ src/auth/infrastructure/auth.repository/mongo/DbAuth.Repository.ts
@@ -0,0 +1,14 @@
+import { authRepository } from "../../../domain/auth.repository";
+import { Login } from "../../../domain/auth.interfaces";
+import { userModel } from "../../../../shared/models/user.model";
+import { User } from "../../../../shared/interfaces/user.interfaces";
+
+export class AuthRepository implements authRepository {
+  async getUser(email: string): Promise<User | null> {
+    const user = await userModel.findOne({
+      email: { $regex: email, $options: "i" },
+    });
+
+    return user;
+  }
+}
