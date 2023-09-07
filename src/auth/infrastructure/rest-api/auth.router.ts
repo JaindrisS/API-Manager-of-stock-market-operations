65ac158279ce88c1cd2a996457eb1a1e54afcbc8
jaindriss <jaindrissosajsd@gmail.com>
2023-09-06 20:53:41 -0500

    feat: add test route to test middlewar

--- src/auth/infrastructure/rest-api/auth.router.ts
+++ src/auth/infrastructure/rest-api/auth.router.ts
@@ -10,6 +10,19 @@ export class AuthRouter extends BaseRouter<AuthController, AuthMiddleware> {
   }
 
   routes(): void {
+    this.router.get(
+      "/test",
+      (req, res, next) => this.middleware.validateToken(req, res, next),
+      (_req, res) => {
+        return res.json("Test Ok");
+      }
+    );
+
     this.router.post("/login", (req, res) => this.controller.login(req, res));
+    this.router.post(
+      "/logout",
+
+      (req, res) => this.controller.logout(req, res)
+    );
   }
 }
