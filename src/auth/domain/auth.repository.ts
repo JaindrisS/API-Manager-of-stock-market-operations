1b41f7cad5b80dcad97df228304ac9a560c85e6e
jaindriss <jaindrissosajsd@gmail.com>
2023-09-09 20:45:34 -0500

    feat: add ResetPassword method

--- src/auth/domain/auth.repository.ts
+++ src/auth/domain/auth.repository.ts
@@ -2,4 +2,5 @@ import { User } from "../../shared/interfaces/user.interfaces";
 
 export interface authRepository {
   getUser(email: string): Promise<User | null>;
+  ResetPassword(id: string, token: string): Promise<User | null>;
 }
