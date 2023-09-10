f481836f3f002da3428d2ef38cf3d1c48414b721
jaindriss <jaindrissosajsd@gmail.com>
2023-09-09 20:42:38 -0500

    feat: add sendmail method

--- src/auth/application/auth.service.ts
+++ src/auth/application/auth.service.ts
@@ -3,6 +3,8 @@ import * as bcrypt from "bcrypt";
 import { HttpResponse } from "../../shared/response/httpResponse";
 import { authRepository } from "../domain/auth.repository";
 import { generateJwt } from "./generateJwt";
+import { getEmailTemplate } from "../infrastructure/template";
+import { sendEmail } from "../infrastructure/sendMail";
 
 const invalidatedTokens = new Set();
 
@@ -42,13 +44,40 @@ export class AuthService {
   }
 
   async logout(token: string, res: Response) {
-
     if (token) {
       await this.blacklistToken(token);
 
       return this.httpResponse.OK(res, "Token invalidated");
     } else {
       return this.httpResponse.BadRequest(res, "No token provided");
+    }
+  }
+
+  async sendMail(email: string, res: Response) {
+    try {
+      const user = await this.authRepository.getUser(email);
+
+      if (!user) {
+        return this.httpResponse.BadRequest(res, "Mail is incorrect");
+      }
+
+      const token = await generateJwt(user?.id);
+
+      const data = { email, token };
+
+      const emailHtmlTemplate = getEmailTemplate(data);
+
+      await sendEmail(email, "Recuperar contrasena", emailHtmlTemplate);
+
+      this.authRepository.ResetPassword(user.id, <string>token);
+
+      return this.httpResponse.OK(res, {
+        email,
+        token,
+        msg: "Email sent successfully!",
+      });
+    } catch (error) {
+      return this.httpResponse.Error(res, error);
     }
   }
 
