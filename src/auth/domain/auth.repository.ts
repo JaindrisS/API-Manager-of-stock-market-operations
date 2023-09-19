7277c14cd87eb80984b758deadeab7c810e9a905
jaindriss <jaindrissosajsd@gmail.com>
2023-09-18 20:31:18 -0500

    feat: add getByToken updatePassword methods

--- src/auth/domain/auth.repository.ts
+++ src/auth/domain/auth.repository.ts
@@ -1,6 +1,9 @@
+import { NullLiteral } from "typescript";
 import { User } from "../../shared/interfaces/user.interfaces";
 
 export interface authRepository {
   getUser(email: string): Promise<User | null>;
-  ResetPassword(id: string, token: string): Promise<User | null>;
+  ResetPassword(id: string, token: string | null): Promise<User | null>;
+  getByToken(token: string): Promise<User | null>;
+  updatePassword(id: string, password: string): Promise<User | null>;
 }
