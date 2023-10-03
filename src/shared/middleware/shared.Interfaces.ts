0344a8091a83147c589b16dea09595a9d233e4d8
jaindriss <jaindrissosajsd@gmail.com>
2023-10-02 21:40:02 -0500

    feat(shard.Interfaces.ts): add logger method to the interface

--- src/shared/middleware/shared.Interfaces.ts
+++ src/shared/middleware/shared.Interfaces.ts
@@ -1,6 +1,8 @@
 import { User } from "../../shared/interfaces/user.interfaces";
 import { Request } from "express";
+import { Logger } from "winston";
 
 export interface RequestWithUserRol extends Request {
-  user?: User|any;
+  user?: User | any;
+  logger?:Logger;
 }
