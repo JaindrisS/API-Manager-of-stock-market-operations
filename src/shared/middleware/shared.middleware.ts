dde771a53aa24890a6f1b4451b90394b9f8acec1
jaindriss <jaindrissosajsd@gmail.com>
2023-10-02 21:37:46 -0500

    feat(share.middleware.ts): add custom logger function to save the request logs

--- src/shared/middleware/shared.middleware.ts
+++ src/shared/middleware/shared.middleware.ts
@@ -1,7 +1,14 @@
 import { Response, NextFunction } from "express";
+import path from "path";
+import { createLogger, format, transports } from "winston";
+import DailyRotateFile from "winston-daily-rotate-file";
 import { RoleType } from "../../user/application/user.dto";
 import { HttpResponse } from "../response/httpResponse";
 import { RequestWithUserRol } from "./shared.Interfaces";
+
+const { combine, timestamp, label, printf } = format;
+
+const logFilePath = path.join("logs/custom-logs");
 
 export class SharedMiddleware {
   public httpResponse: HttpResponse;
@@ -28,4 +35,37 @@ export class SharedMiddleware {
 
     return next;
   }
+
+  customLoggerMiddleware = (
+    req: RequestWithUserRol,
+    _res: Response,
+    next: NextFunction
+  ) => {
+    const myFormat = printf(({ level, message, timestamp }) => {
+      return `${timestamp} ${level}: ${message} `;
+    });
+
+    req.logger = createLogger({
+      level: "info",
+      format: combine(
+        format.colorize(),
+        label({ label: "right meow!" }),
+        timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+        myFormat
+      ),
+
+      transports: [
+        new transports.Console(),
+        new DailyRotateFile({
+          filename: logFilePath,
+          datePattern: "YYYY-MM-DD",
+          zippedArchive: true,
+          maxSize: "5m",
+          maxFiles: "1d",
+        }),
+      ],
+    });
+
+    next();
+  };
 }
