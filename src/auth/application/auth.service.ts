e6cbfb96000dbfbd9aac47cebbe89afcf67fc70d
jaindriss <jaindrissosajsd@gmail.com>
2023-10-04 18:12:20 -0500

    feat(auth.service.ts): add logger to possible responses

--- src/auth/application/auth.service.ts
+++ src/auth/application/auth.service.ts
@@ -5,7 +5,7 @@ import { authRepository } from "../domain/auth.repository";
 import { generateJwt } from "./generateJwt";
 import { LoginDto } from "../domain/auth.dto";
 import { TokenService } from "./token.service";
-
+import { RequestWithUserRol } from "../../shared/middleware/shared.Interfaces";
 
 export class AuthService {
   private readonly authRepository: authRepository;
@@ -22,23 +22,36 @@ export class AuthService {
     this.tokenService = tokenService;
   }
 
-  async login(data: LoginDto, res: Response) {
+  async login(data: LoginDto, req: RequestWithUserRol, res: Response) {
     const { email, password } = data;
+    const { originalUrl, ip, method } = req;
 
     try {
       const user = await this.authRepository.getUser(email);
 
+      const errorMsg = "Invalid email or password";
+
       if (user?.password === undefined || user?.email === undefined) {
-        return this.httpResponse.BadRequest(res, "Invalid email or password");
+        req.logger?.info(` ${errorMsg} -  Url:${originalUrl}`);
+
+        return this.httpResponse.BadRequest(res, errorMsg);
       }
 
       const comparePassword = bcrypt.compareSync(password, user?.password);
 
       if (!user?.status || !user || !comparePassword) {
+        req.logger?.info(
+          `MSG: ${errorMsg}  -  URL: ${method} ${originalUrl} - IP: ${ip} - StatusCode: ${400} `
+        );
+
         return this.httpResponse.BadRequest(res, "Invalid email or password");
       }
 
       const token = await generateJwt(user.id);
+
+      req.logger?.info(
+        `MSG: Login ok  -  URL: ${method} ${originalUrl} - IP: ${ip} - StatusCode: ${200} `
+      );
 
       return this.httpResponse.OK(res, {
         msg: "Login ok",
@@ -46,6 +59,10 @@ export class AuthService {
         token: token,
       });
     } catch (error) {
+      req.logger?.error(
+        `MSG: Error  -  URL: ${method} ${originalUrl} - IP: ${ip} - StatusCode: ${500} `
+      );
+
       return this.httpResponse.Error(res, error);
     }
   }
@@ -59,6 +76,4 @@ export class AuthService {
       return this.httpResponse.BadRequest(res, "No token provided");
     }
   }
-
-
 }
