758af381f1d4583f9f173783b59a866db368bbbf
jaindriss <jaindrissosajsd@gmail.com>
2023-10-04 18:28:27 -0500

    feat(shared.interfaces.ts): change file to another directory

--- src/shared/middleware/shared.Interfaces.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "../../shared/interfaces/user.interfaces";
-import { Request } from "express";
-import { Logger } from "winston";
-
-export interface RequestWithUserRol extends Request {
-  user?: User | any;
-  logger?:Logger;
-}
