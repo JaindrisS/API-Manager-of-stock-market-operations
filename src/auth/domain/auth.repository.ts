8dc8222e0eeee87f32979505f3e33ac3ba166492
jaindriss <jaindrissosajsd@gmail.com>
2023-10-11 06:43:55 -0500

    feat(auth.repository.ts): add updateUnsuccessfulAttempts

--- src/auth/domain/auth.repository.ts
+++ src/auth/domain/auth.repository.ts
@@ -6,4 +6,10 @@ export interface authRepository {
   ResetPassword(id: string, token: string | null): Promise<User | null>;
   getByToken(token: string): Promise<User | null>;
   updatePassword(id: string, password: string): Promise<User | null>;
+  updateUnsuccessfulAttempts(
+    id: string,
+    number: number | null,
+    newValue: number | null
+  ): Promise<unknown>;
+  blockedUntil(id: string, number: number): Promise<unknown>;
 }
